78e2d840ee16f1d4e517e5aa853db1dbc1089382
Ivan Vasquez <ivancamilov@gmail.com>
2015-06-01 15:59:11 -0500

    basic structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -10,14 +11,25 @@ gulp.task('serve', function() {
   });
 });
 
+gulp.task('sass', function() {
+  return gulp.src('sass/*.scss')
+      .pipe(sass({
+        sourcemap: true,
+        includePaths: require('node-neat').includePaths
+      }))
+      .pipe(gulp.dest('css'))
+      .pipe(reload({stream:true}));
+});
+
 gulp.task('reload', function() {
   browserSync.reload();
 });
 
 gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', { interval: 500 }, ['sass']);
   gulp.watch('css/**/*.css', { interval: 500 }, ['reload']);
   gulp.watch('js/**/*.js', { interval: 500 }, ['reload']);
   gulp.watch('*.html', { interval: 500 }, ['reload']);
 });
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('default', ['sass', 'serve', 'watch']);
