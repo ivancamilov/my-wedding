0af47ffba116d452c591d58275399884cf0900e8
Ivan Vasquez <ivancamilov@gmail.com>
2015-07-26 22:29:57 -0500

    saltando al infinito y más allá

--- js/main.js
+++ js/main.js
@@ -19,12 +19,51 @@ function update() {
   ticking = false;
   var currentScrollY = latestKnownScrollY;
   var headerOffset = document.getElementById('we-meet').getBoundingClientRect().top;
+  var jumpingHenOffset = document.getElementById('hen-jump-trigger').getBoundingClientRect().top;
+  var flyingHenOffset = document.getElementById('flying-jump-trigger').getBoundingClientRect().top;
+  var staticOffset = document.getElementById('static-trigger').getBoundingClientRect().top;
 
   if(headerOffset <= 0) {
     document.getElementById('header').className = 'visible';
   }
   else {
     document.getElementById('header').className = '';
+  }
+
+  var jumpingHen = document.getElementById('jumping-hen');
+
+  if(staticOffset <= 0) {
+    jumpingHen.className = 'jumping-hen flying static';
+  }
+  else if(jumpingHenOffset <= 0) {
+
+    var flyAnimation = function(e) {
+      jumpingHen.removeEventListener('animationend', flyAnimation);
+
+      if(jumpingHen.className == 'jumping-hen falling-down') {
+        document.getElementById('jumping-hen').className = 'jumping-hen falling-down flying';
+      }
+    };
+
+    var fallAnimation = function(e) {
+      jumpingHen.removeEventListener('animationend', fallAnimation);
+      if(jumpingHen.className == 'jumping-hen doing-jump') {
+        document.getElementById('jumping-hen').src = 'img/flying-hen.svg';
+        document.getElementById('jumping-hen').className = 'jumping-hen falling-down';
+
+        jumpingHen.addEventListener('animationend', flyAnimation);
+      }
+    };
+
+    if(jumpingHen.className.indexOf('f') == -1 && jumpingHen.className.indexOf('static') == -1) {
+      jumpingHen.src = 'img/jumping-hen.svg';
+      jumpingHen.className = 'jumping-hen doing-jump';
+      jumpingHen.addEventListener('animationend', fallAnimation);
+    }
+  }
+  else {
+    document.getElementById('jumping-hen').src = 'img/jumping-hen.svg';
+    document.getElementById('jumping-hen').className = 'jumping-hen';
   }
 }
 
